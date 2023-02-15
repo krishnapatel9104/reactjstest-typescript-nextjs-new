560c88680e255f964d1f26d51cfd486b8b4008a8
krishnapatel9104 <krishna.patel@propelius.tech>
2023-02-15 23:40:47 +0530

    change in home compoenent

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,12 +1,13 @@
-import { Box } from '@mui/material';
-import React from 'react'
-import Navbar from '../src/components/Navbar';
+import { Box } from "@mui/material";
+import React from "react";
+import Navbar from "../src/components/Navbar";
 const Home = () => {
-  return (
-    <Box>Home
-      <Navbar/>
-    </Box>
-  )
-}
+    return (
+        <Box>
+            Home Component
+            <Navbar />
+        </Box>
+    );
+};
 
 export default Home;
