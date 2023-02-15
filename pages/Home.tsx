8a8ba3ad79e75cd8c3bd4a762e99329ee217d3f5
krishnapatel9104 <krishna.patel@propelius.tech>
2023-02-15 23:29:50 +0530

    project setup of nextjs and typescript

---
+++ pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import React from 'react'
+import Navbar from '../src/components/Navbar';
+const Home = () => {
+  return (
+    <Box>Home
+      <Navbar/>
+    </Box>
+  )
+}
+
+export default Home;
