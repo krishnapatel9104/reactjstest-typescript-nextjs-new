2e79ae5182ba6dab505a7de0013ce3e9b752ff43
krishnapatel9104 <krishna.patel@propelius.tech>
2023-02-21 13:24:25 +0530

    shopbycategorysection-add no match found

--- src/components/Home/ShopByCategory.tsx
+++ src/components/Home/ShopByCategory.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Button, Typography, Box, Link } from '@mui/material';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -12,9 +12,11 @@ import { categoryLists } from '../../data/categoryLists';
 import theme from '../../theme';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { productsType } from '../../types/constants/products.type';
 
 export const ShopByCategory = () => {
   const router = useRouter();
+  const [filterProductLists, setFilterProductLists] = useState<productsType[]>([]);
   const [selectedGender, setSelectedGender] = useState<number>(genderLists[0].id);
   const [selectedCategory, setSelectedCategory] = useState<number>(categoryLists[0].id);
   const [swiperCurrentIndex, setSwiperCurrentIndex] = useState<number>(0);
@@ -31,6 +33,13 @@ export const ShopByCategory = () => {
       }
     }
   };
+
+  useEffect(() => {
+    let data = productLists.filter(
+      product => product.gender === selectedGender && product.category === selectedCategory
+    );
+    setFilterProductLists(data);
+  }, [selectedGender, selectedCategory]);
 
   return (
     <Box>
@@ -139,213 +148,226 @@ export const ShopByCategory = () => {
             color: '#D1D1D6'
           }
         }}>
-        <Box
-          className="swiper-button image-swiper-button-prev"
-          sx={{
-            height: {
-              xs: '15px',
-              sm: '20px',
-              md: '20px',
-              lg: '30px'
-            },
-            width: {
-              xs: '50px',
-              sm: '30px',
-              md: '25px',
-              lg: '25px'
-            },
-            opacity: swiperCurrentIndex === 0 ? 0.2 : 1,
-            marginTop: '-90px'
-          }}
-          ref={shopByCategoryPrevRef}>
-          <Image
-            src={'/images/vectorLeft.png'}
-            alt="imageGirl"
-            height={0}
-            width={0}
-            sizes="(max-width:0) 100vw,
+        {filterProductLists.length > 0 && (
+          <>
+            <Box
+              className="swiper-button image-swiper-button-prev"
+              sx={{
+                height: {
+                  xs: '15px',
+                  sm: '20px',
+                  md: '20px',
+                  lg: '30px'
+                },
+                width: {
+                  xs: '50px',
+                  sm: '30px',
+                  md: '25px',
+                  lg: '25px'
+                },
+                opacity: swiperCurrentIndex === 0 ? 0.2 : 1,
+                marginTop: '-90px'
+              }}
+              ref={shopByCategoryPrevRef}>
+              <Image
+                src={'/images/vectorLeft.png'}
+                alt="imageGirl"
+                height={0}
+                width={0}
+                sizes="(max-width:0) 100vw,
                     (max-height:0) 100vh"
-            style={{
-              objectFit: 'contain',
-              height: '100%',
-              width: '100%'
-            }}
-          />
-        </Box>
-        <Swiper
-          onReachBeginning={e => setSwiperCurrentIndex(0)}
-          onReachEnd={e => setSwiperCurrentIndex(1)}
-          slidesPerView={4}
-          centeredSlides={false}
-          slidesPerGroupSkip={1}
-          grabCursor={true}
-          keyboard={{
-            enabled: true
-          }}
-          onBeforeInit={onBeforeInit}
-          navigation={{
-            prevEl: shopByCategoryPrevRef.current,
-            nextEl: shopByCategoryNextRef.current,
-            disabledClass: 'swiper-button-disabled'
-          }}
-          breakpoints={{
-            1280: {
-              slidesPerView: 4,
-              slidesPerGroup: 1
-            },
-            960: {
-              slidesPerView: 3,
-              slidesPerGroup: 1
-            },
-            600: {
-              slidesPerView: 2,
-              slidesPerGroup: 1
-            },
-            0: {
-              slidesPerView: 1,
-              slidesPerGroup: 1
-            }
-          }}
-          modules={[Keyboard, Scrollbar, Navigation, Pagination]}
-          className="mySwiper">
-          {productLists.length > 0 ? (
-            productLists.map(product => {
-              let gender = genderLists.find(gender => gender.id === selectedGender);
-              let category = categoryLists.find(category => category.id === selectedCategory);
-              if (product.category === selectedCategory && product.gender === selectedGender) {
-                return (
-                  <Box key={product.id}>
-                    <SwiperSlide
-                      style={{
-                        fontSize: '18px',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                      }}
-                      onClick={() =>
-                        router.push(`${gender?.slug}/products/category/${category?.slug}`)
-                      }>
-                      <Box
-                        sx={{
-                          marginLeft: '10px',
-                          gap: '50px',
-                          display: 'flex',
-                          flexDirection: 'column'
-                        }}>
-                        <Box
-                          sx={{
-                            width: {
-                              xs: '150px',
-                              lg: '170px',
-                              xl: '300px'
-                            },
-                            height: {
-                              xs: '150px',
-                              lg: '170px',
-                              xl: '300px'
-                            },
-                            gap: '30px',
-                            display: 'flex',
-                            flexDirection: 'column'
-                          }}>
-                          <Image
-                            src={product.productImages[0].productImage}
-                            alt="imageGirl"
-                            height={0}
-                            width={0}
-                            sizes="(max-width:0) 100vw,
-                                      (max-height:0) 100vh"
-                            style={{
-                              objectFit: 'contain',
-                              height: '100%',
-                              width: '100%'
-                            }}
-                          />
-                        </Box>
-                        <Box>
-                          <Typography
-                            sx={{
-                              fontFamily: theme.typography.h2.fontFamily,
-                              fontWeight: '700',
-                              fontSize: {
-                                lg: '22px',
-                                md: '20px',
-                                xs: '18px'
-                              },
-                              color: theme.palette.primary.main
-                            }}>
-                            {product.productName}
-                          </Typography>
-                          <Typography>
-                            <span
-                              style={{
-                                fontFamily: theme.typography.h2.fontFamily,
-                                fontWeight: '400',
-                                fontSize: '20px',
-                                textDecorationLine: 'line-through',
-                                color: theme.palette.secondary.light
-                              }}>
-                              ${product.productOriginalPrice}
-                            </span>
-                            &nbsp;&nbsp;
-                            <span
-                              className="currentprice"
-                              style={{
-                                fontFamily: theme.typography.h2.fontFamily,
-                                fontWeight: '400',
-                                fontSize: '20px',
-                                color: theme.palette.secondary.dark
-                              }}>
-                              ${product.productCurrentPrice}
-                            </span>
-                          </Typography>
-                        </Box>
-                      </Box>
-                    </SwiperSlide>
-                  </Box>
-                );
-              }
-            })
-          ) : (
-            <Box sx={{ backgroundColor: 'red', height: '20px', width: '20px' }}>
-              No Product Match
+                style={{
+                  objectFit: 'contain',
+                  height: '100%',
+                  width: '100%'
+                }}
+              />
             </Box>
-          )}
-        </Swiper>
-        <Box
-          className="swiper-button image-swiper-button-next"
-          sx={{
-            height: {
-              xs: '15px',
-              sm: '20px',
-              md: '20px',
-              lg: '30px'
-            },
-            width: {
-              xs: '50px',
-              sm: '30px',
-              md: '25px',
-              lg: '25px'
-            },
-            opacity: swiperCurrentIndex === 1 ? 0.2 : 1,
-            marginTop: '-90px'
-          }}
-          ref={shopByCategoryNextRef}>
-          <Image
-            src={'/images/vectorRight.png'}
-            alt="imageGirl"
-            height={0}
-            width={0}
-            sizes="(max-width:0) 100vw,
+            <Swiper
+              onReachBeginning={e => setSwiperCurrentIndex(0)}
+              onReachEnd={e => setSwiperCurrentIndex(1)}
+              slidesPerView={4}
+              centeredSlides={false}
+              slidesPerGroupSkip={1}
+              grabCursor={true}
+              keyboard={{
+                enabled: true
+              }}
+              onBeforeInit={onBeforeInit}
+              navigation={{
+                prevEl: shopByCategoryPrevRef.current,
+                nextEl: shopByCategoryNextRef.current,
+                disabledClass: 'swiper-button-disabled'
+              }}
+              breakpoints={{
+                1280: {
+                  slidesPerView: 4,
+                  slidesPerGroup: 1
+                },
+                960: {
+                  slidesPerView: 3,
+                  slidesPerGroup: 1
+                },
+                600: {
+                  slidesPerView: 2,
+                  slidesPerGroup: 1
+                },
+                0: {
+                  slidesPerView: 1,
+                  slidesPerGroup: 1
+                }
+              }}
+              modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+              className="mySwiper">
+              {filterProductLists.length > 0 ? (
+                filterProductLists.map(product => {
+                  let gender = genderLists.find(gender => gender.id === selectedGender);
+                  let category = categoryLists.find(category => category.id === selectedCategory);
+                  if (product.category === selectedCategory && product.gender === selectedGender) {
+                    return (
+                      <Box key={product.id}>
+                        <SwiperSlide
+                          style={{
+                            fontSize: '18px',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                          }}
+                          onClick={() =>
+                            router.push(`${gender?.slug}/products/category/${category?.slug}`)
+                          }>
+                          <Box
+                            sx={{
+                              marginLeft: '10px',
+                              gap: '50px',
+                              display: 'flex',
+                              flexDirection: 'column'
+                            }}>
+                            <Box
+                              sx={{
+                                width: {
+                                  xs: '150px',
+                                  lg: '170px',
+                                  xl: '300px'
+                                },
+                                height: {
+                                  xs: '150px',
+                                  lg: '170px',
+                                  xl: '300px'
+                                },
+                                gap: '30px',
+                                display: 'flex',
+                                flexDirection: 'column'
+                              }}>
+                              <Image
+                                src={product.productImages[0].productImage}
+                                alt="imageGirl"
+                                height={0}
+                                width={0}
+                                sizes="(max-width:0) 100vw,
+                                      (max-height:0) 100vh"
+                                style={{
+                                  objectFit: 'contain',
+                                  height: '100%',
+                                  width: '100%'
+                                }}
+                              />
+                            </Box>
+                            <Box>
+                              <Typography
+                                sx={{
+                                  fontFamily: theme.typography.h2.fontFamily,
+                                  fontWeight: '700',
+                                  fontSize: {
+                                    lg: '22px',
+                                    md: '20px',
+                                    xs: '18px'
+                                  },
+                                  color: theme.palette.primary.main
+                                }}>
+                                {product.productName}
+                              </Typography>
+                              <Typography>
+                                <span
+                                  style={{
+                                    fontFamily: theme.typography.h2.fontFamily,
+                                    fontWeight: '400',
+                                    fontSize: '20px',
+                                    textDecorationLine: 'line-through',
+                                    color: theme.palette.secondary.light
+                                  }}>
+                                  ${product.productOriginalPrice}
+                                </span>
+                                &nbsp;&nbsp;
+                                <span
+                                  className="currentprice"
+                                  style={{
+                                    fontFamily: theme.typography.h2.fontFamily,
+                                    fontWeight: '400',
+                                    fontSize: '20px',
+                                    color: theme.palette.secondary.dark
+                                  }}>
+                                  ${product.productCurrentPrice}
+                                </span>
+                              </Typography>
+                            </Box>
+                          </Box>
+                        </SwiperSlide>
+                      </Box>
+                    );
+                  }
+                })
+              ) : (
+                <></>
+              )}
+            </Swiper>
+            <Box
+              className="swiper-button image-swiper-button-next"
+              sx={{
+                height: {
+                  xs: '15px',
+                  sm: '20px',
+                  md: '20px',
+                  lg: '30px'
+                },
+                width: {
+                  xs: '50px',
+                  sm: '30px',
+                  md: '25px',
+                  lg: '25px'
+                },
+                opacity: swiperCurrentIndex === 1 ? 0.2 : 1,
+                marginTop: '-90px'
+              }}
+              ref={shopByCategoryNextRef}>
+              <Image
+                src={'/images/vectorRight.png'}
+                alt="imageGirl"
+                height={0}
+                width={0}
+                sizes="(max-width:0) 100vw,
                     (max-height:0) 100vh"
-            style={{
-              objectFit: 'contain',
-              height: '100%',
-              width: '100%'
-            }}
-          />
-        </Box>
+                style={{
+                  objectFit: 'contain',
+                  height: '100%',
+                  width: '100%'
+                }}
+              />
+            </Box>
+          </>
+        )}
       </Box>
+      {filterProductLists.length === 0 && (
+        <Typography
+          sx={{
+            fontSize: '22px',
+            fontWeight: '700',
+            textAlign: 'center',
+            marginTop: '0px'
+          }}>
+          No Product Match
+        </Typography>
+      )}
       <Box sx={{ textAlign: 'center' }}>
         <Button
           variant="contained"
